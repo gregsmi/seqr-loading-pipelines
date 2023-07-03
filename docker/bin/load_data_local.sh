57da20eb2119de7f54b34b7f512119fdb116d2ce
Greg Smith <gregsmi@microsoft.com>
2023-07-03 17:07:47 +0000

    moved luigi params to config

--- docker/bin/load_data_local.sh
+++ docker/bin/load_data_local.sh
@@ -2,36 +2,20 @@
 
 set -x -e
 
-BUILD_VERSION=$1
-SAMPLE_TYPE=$2
-INDEX_NAME=$3
-SOURCE_FILE=$4
-REF_HT_PATH=$5
-CLINVAR_HT_PATH=$6
-VEP_CONFIG_PATH=$7
-GRCH38_TO_GRCH37_REF_PATH=$8
+SAMPLE_TYPE=$1
+INDEX_NAME=$2
+SOURCE_FILE=$3
 
 DEST_FILE="${SOURCE_FILE/.*/}".mt
 
+# additional params loaded from Luigi config files
 python3 -m seqr_loading SeqrVCFToMTTask --local-scheduler \
     --source-paths "${SOURCE_FILE}" \
     --dest-path "${DEST_FILE}" \
-    --genome-version "${BUILD_VERSION}" \
     --sample-type "${SAMPLE_TYPE}" \
-    --reference-ht-path "${REF_HT_PATH}" \
-    --clinvar-ht-path "${CLINVAR_HT_PATH}" \
-    --vep-config-json-path "${VEP_CONFIG_PATH}" \
-    --grch38-to-grch37-ref-chain "${GRCH38_TO_GRCH37_REF_PATH}" \
     --vep-runner "DUMMY"
 
 # python3 -m seqr_loading SeqrMTToESTask --local-scheduler \
-#     --reference-ht-path "/seqr-reference-data/${FULL_BUILD_VERSION}/combined_reference_data_grch${BUILD_VERSION}.ht" \
-#     --clinvar-ht-path "/seqr-reference-data/${FULL_BUILD_VERSION}/clinvar.${FULL_BUILD_VERSION}.ht" \
-#     --vep-config-json-path "/vep_configs/vep-${FULL_BUILD_VERSION}-loftee.json" \
 #     --es-host elasticsearch \
 #     --es-index-min-num-shards 1 \
-#     --sample-type "${SAMPLE_TYPE}" \
 #     --es-index "${INDEX_NAME}" \
-#     --genome-version "${BUILD_VERSION}" \
-#     --source-paths "${SOURCE_FILE}" \
-#     --dest-path "${DEST_FILE}"
--- run_load_local.sh
+++ run_load_local.sh
@@ -3,25 +3,20 @@
 set -x -e
 
 BUILD_VERSION=38
-FULL_BUILD_VERSION=GRCh38
 SAMPLE_TYPE=WGS
 INDEX_NAME=gregsmi-test
-INPUT_FILE_PATH=GRCh38/prior_to_annotation.vcf.bgz
-SOURCE_FILE=/input_vcfs/${INPUT_FILE_PATH}
+SOURCE_FILE=/input_vcfs/GRCh38/prior_to_annotation.vcf.bgz
 
-# REFERENCE_DATA_BUCKET="hail-az://azcpg001sa/reference"
-# REFERENCE_DATA_BUCKET="https://azcpg001sa.blob.core.windows.net/reference"
-REFERENCE_DATA_BUCKET="abfss://reference@azcpg001sa.dfs.core.windows.net"
-REF_HT_PATH="${REFERENCE_DATA_BUCKET}/seqr/v0-1/combined_reference_data_grch${BUILD_VERSION}.ht"
-CLINVAR_HT_PATH="${REFERENCE_DATA_BUCKET}/seqr/v0-1/clinvar.${FULL_BUILD_VERSION}.ht"
-VEP_CONFIG_PATH="/vep_configs/vep-GRCh38-loftee.json"
-GRCH38_TO_GRCH37_REF_PATH="${REFERENCE_DATA_BUCKET}/hail/grch38_to_grch37.over.chain.gz"
+# Fill in /run-config/core-sites.xml with necessary Spark ABFS authentication settings/secrets. 
+# Fill in /run-config/refs.*.cfg with appropriate reference paths.
+# Tell Spark and Luigi to use the mounted directory for configuration.
 docker run -it \
   -v $(pwd)/.data/seqr-reference-data:/seqr-reference-data \
   -v $(pwd)/.data/vep_data:/vep_data \
   -v $(pwd)/.data/input_vcfs:/input_vcfs \
-  -v $(pwd)/spark-config:/spark-config \
+  -v $(pwd)/run-config:/run-config \
+  -e LUIGI_CONFIG_PATH=/run-config/refs.${BUILD_VERSION}.cfg \
+  -e SPARK_CONF_DIR=/run-config \
   pipeline-runner-ms:latest \
-  load_data_local.sh $BUILD_VERSION $SAMPLE_TYPE $INDEX_NAME $SOURCE_FILE \
-    $REF_HT_PATH $CLINVAR_HT_PATH $VEP_CONFIG_PATH $GRCH38_TO_GRCH37_REF_PATH
+  load_data_local.sh $SAMPLE_TYPE $INDEX_NAME $SOURCE_FILE
 
