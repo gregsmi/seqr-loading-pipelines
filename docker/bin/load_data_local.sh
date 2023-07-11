73486a99b8889114571e25f702ee2e4dadd2e390
Greg Smith <gregsmi@microsoft.com>
2023-07-11 22:50:26 +0000

    rearrange configs

--- docker/bin/load_data_local.sh
+++ docker/bin/load_data_local.sh
@@ -4,8 +4,12 @@ set -x -e
 
 SOURCE_FILE=$1
 DEST_FILE=$2
-SAMPLE_TYPE=$3
-INDEX_NAME=$4
+BUILD_VERSION=$3
+SAMPLE_TYPE=$4
+INDEX_NAME=$5
+
+# Point Luigi to the correct mounted config file for default args.
+export LUIGI_CONFIG_PATH=/luigi_configs/seqr-loading-GRCh${BUILD_VERSION}.cfg
 
 # additional params loaded from Luigi config files
 python3 -m seqr_loading SeqrVCFToMTTask --local-scheduler \
--- run_load_local.sh
+++ run_load_local.sh
@@ -9,13 +9,8 @@ SOURCE_FILE=abfss://data@azcpg001sa.dfs.core.windows.net/GRCh38/1kg_sample_test.
 DEST_FILE=abfss://data@azcpg001sa.dfs.core.windows.net/GRCh38/1kg_sample_test.mt
 
 # Fill in .config/core-sites.xml with necessary Spark ABFS authentication settings/secrets. 
-# Fill in .config/refs.*.cfg with appropriate reference paths.
-# Tell Spark and Luigi to use the mounted directory for configuration.
 docker run -it \
+  -v ./.config/core-site.xml:/spark_configs/core-site.xml \
   -v ./.data/vep_data:/vep_data \
-  -v ./.config:/config \
-  -e LUIGI_CONFIG_PATH=/config/seqr-loading-GRCh${BUILD_VERSION}.cfg \
-  -e SPARK_CONF_DIR=/config \
   pipeline-runner:azure \
-  load_data_local.sh $SOURCE_FILE $DEST_FILE $SAMPLE_TYPE $INDEX_NAME
-
+  load_data_local.sh $SOURCE_FILE $DEST_FILE $BUILD_VERSION $SAMPLE_TYPE $INDEX_NAME
