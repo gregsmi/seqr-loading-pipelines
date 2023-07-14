c5253933023f73d3a10d6cb36e6dea25226b0573
Greg Smith <gregsmi@microsoft.com>
2023-07-14 19:55:39 +0000

    updated run script

--- run_load_local.sh
+++ run_load_local.sh
@@ -4,11 +4,13 @@ set -x -e
 
 BUILD_VERSION=38
 SAMPLE_TYPE=WGS
-INDEX_NAME=gregsmi-test
+INDEX_NAME=testdata
 SOURCE_FILE=abfss://data@azcpg001sa.dfs.core.windows.net/GRCh38/1kg_sample_test.vcf.bgz
 DEST_FILE=abfss://data@azcpg001sa.dfs.core.windows.net/GRCh38/1kg_sample_test.mt
 
 # Fill in .config/core-sites.xml with necessary Spark ABFS authentication settings/secrets. 
+# Download necessary VEP reference data to .data/vep_data.
+
 docker run -it \
   -v ./.config/core-site.xml:/spark_configs/core-site.xml \
   -v ./.data/vep_data:/vep_data \
